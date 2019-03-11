a54577888aef57b3e55192a7155b9fdad6136f87
Mike Joyce <michael.joyce@target.com>
2019-03-11 06:47:47 -0500

    remove dead link

--- src/pages/Home/HomePage.tsx
+++ src/pages/Home/HomePage.tsx
@@ -13,9 +13,6 @@ const HomePage = () => {
 					<Link to="/plan">See All Plans</Link>
 				</li>
 				<li>
-					<Link to="/plan/34384hfds">Select a plan</Link>
-				</li>
-				<li>
 					<Link to="/repositioning">Reorder feature (WIP)</Link>
 				</li>
 			</ul>
