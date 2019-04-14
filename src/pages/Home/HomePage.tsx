7e4f58542046caa7253aa1777a7376339a21ee8e
Mike Joyce <michael.joyce@target.com>
2019-04-13 17:46:19 -0700

    get graphql setup

--- src/pages/Home/HomePage.tsx
+++ src/pages/Home/HomePage.tsx
@@ -1,16 +1,37 @@
 import React from 'react'
 import Typography from '@material-ui/core/Typography'
+import ApolloClient, { gql } from 'apollo-boost'
+import { ApolloProvider, Query } from 'react-apollo'
+
+const client = new ApolloClient({
+	uri: '/.netlify/functions/graphql'
+})
+
+const query = gql`
+	{
+		hello
+	}
+`
 
 const HomePage = () => {
 	return (
-		<div>
-			<Typography variant="h2">Hangry?</Typography>
+		<ApolloProvider client={client}>
+			<Query query={query}>
+				{({ data }) => {
+					console.log(data)
 
-			<Typography variant="body1">
-				Welcome to meal plan site! This site is ugly and isn't fully
-				functional...but it will be some day!
-			</Typography>
-		</div>
+					return <div>{JSON.stringify(data, null, 2)}</div>
+				}}
+			</Query>
+			<div>
+				<Typography variant="h2">Hangry?</Typography>
+
+				<Typography variant="body1">
+					Welcome to meal plan site! This site is ugly and isn't fully
+					functional...but it will be some day!
+				</Typography>
+			</div>
+		</ApolloProvider>
 	)
 }
 
