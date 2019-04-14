41565e168d78b6261f4a79a62de87e59ee79faf2
Mike Joyce <michael.joyce@target.com>
2019-04-14 11:43:39 -0700

    nodemon it. add schema

--- src/lambda/graphql.js
+++ src/lambda/graphql.js
@@ -1,16 +1,107 @@
 const { ApolloServer, gql } = require('apollo-server-lambda')
+const MealService = require('./meal_service')
 
 const typeDefs = gql`
 	type Query {
-		hello: String
+		meals(filter: MealFilter): [Meal!]!
+		mealPlans: [MealPlan!]!
+		mealPlan(mealPlanId: String!): MealPlan
+	}
+
+	input MealFilter {
+		filterType: FilterType
+		ids: [String!]
+	}
+
+	enum FilterType {
+		INCLUDE
+		EXCLUDE
+	}
+
+	type Meal {
+		id: ID!
+		mealName: String!
+		persuasion: Persuasion!
+		protein: Protein!
+		health: Health!
+	}
+
+	type MealPlan {
+		id: ID!
+		mealPlanName: String!
+		numberOfMeals: Int!
+		meals: [Meal!]!
+	}
+
+	enum Persuasion {
+		ITALIAN
+		ASIAN
+		COMFORT
+		TEXMEX
+		INDIAN
+	}
+
+	enum Protein {
+		PORK
+		CHICKEN
+		VEGETARIAN
+		FLEXIBLE
+		BEEF
+	}
+
+	enum Health {
+		EVERYDAY
+		SPLURGE
+		HEALTHY
 	}
 `
 
 const resolvers = {
 	Query: {
-		hello: (root, args, context) => {
-			return 'Hello, world! ths is dope'
+		meals: (_, { filter }, context) => {
+			return context.dataSources.mealApi.getMealsWithFilter(
+				filter.ids,
+				filter.filterType === 'INCLUDE'
+			)
+		},
+		mealPlans: (_, __, context) => {
+			return context.dataSources.mealApi.getMealPlans()
+		},
+		mealPlan: (_, { mealPlanId }, context) => {
+			return context.dataSources.mealApi.getMealPlanById(mealPlanId)
 		}
+	},
+	MealPlan: {
+		numberOfMeals: (parent, _) => {
+			return parent.mealIds.length
+		},
+		meals: (parent, _, context) => {
+			console.log(parent.mealIds)
+			return parent.mealIds.map(id =>
+				context.dataSources.mealApi.getMealById(id)
+			)
+		}
+	},
+	Persuasion: {
+		ITALIAN: 'Italian',
+		ASIAN: 'Asian',
+		COMFORT: 'Comfort',
+		TEXMEX: 'TexMex',
+		INDIAN: 'Indian'
+	},
+
+	Health: {
+		EVERYDAY: 'Everyday',
+		SPLURGE: 'Splurge',
+		HEALTHY: 'Healthy'
+	},
+
+	Protein: {
+		PORK: 'Pork',
+		CHICKEN: 'Chicken',
+		VEGETARIAN: 'Vegetarian',
+		FLEXIBLE: 'Flexible',
+		BEEF: 'Beef'
 	}
 }
 
@@ -18,7 +109,12 @@ const server = new ApolloServer({
 	introspection: true,
 	playground: true,
 	typeDefs,
-	resolvers
+	resolvers,
+	dataSources: () => {
+		return {
+			mealApi: new MealService()
+		}
+	}
 })
 
 exports.handler = server.createHandler()
