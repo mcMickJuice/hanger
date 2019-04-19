b4214c0de9566385162038c65e71ebfa3def6d57
Mike Joyce <michael.joyce@target.com>
2019-04-19 15:32:56 -0500

    remove all unused code

--- src/index.tsx
+++ src/index.tsx
@@ -8,30 +8,24 @@ import MealSelectPage from './pages/MealSelect/MealSelectPage'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import HomePage from './pages/Home/HomePage'
 import MealPlanPage from './pages/MealPlan/MealPlanPage'
-import MealPositioningPage from './features/repositioning/MealPositioningPage'
 import MealPlansPage from './pages/MealPlans/MealPlansPage'
 import Nav from './shared/Nav'
 
 const App = () => {
-	return (
-		<Router>
-			<Nav>
-				<ApolloProvider client={apolloClient}>
-					<Switch>
-						<Route path="/" exact component={HomePage} />
-						<Route path="/build" exact component={MealSelectPage} />
-						<Route path="/plan" exact component={MealPlansPage} />
-						<Route path="/plan/:planId" exact component={MealPlanPage} />
-						<Route
-							path="/repositioning"
-							exact
-							component={MealPositioningPage}
-						/>
-					</Switch>
-				</ApolloProvider>
-			</Nav>
-		</Router>
-	)
+  return (
+    <Router>
+      <Nav>
+        <ApolloProvider client={apolloClient}>
+          <Switch>
+            <Route path="/" exact component={HomePage} />
+            <Route path="/build" exact component={MealSelectPage} />
+            <Route path="/plan" exact component={MealPlansPage} />
+            <Route path="/plan/:planId" exact component={MealPlanPage} />
+          </Switch>
+        </ApolloProvider>
+      </Nav>
+    </Router>
+  )
 }
 
 const rootElement = document.getElementById('root')
