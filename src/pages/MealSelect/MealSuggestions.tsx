aa2668f1d914f58e999c856767d8c3e51b644f9d
Mike Joyce <michael.joyce@target.com>
2019-04-19 15:35:49 -0500

    update query to limit to 9 meals

--- src/pages/MealSelect/MealSuggestions.tsx
+++ src/pages/MealSelect/MealSuggestions.tsx
@@ -8,86 +8,80 @@ import Meal from '../MealSelect/Meal'
 import { Button } from '@material-ui/core'
 
 interface Props {
-	keptMealIds: string[]
-	onKeepMeal: (mealId: string) => void
+  keptMealIds: string[]
+  onKeepMeal: (mealId: string) => void
 }
 
 const query = gql`
-	query GetMealSuggestions($excludeIds: [ID!]!) {
-		meals(filter: { filterType: EXCLUDE, ids: $excludeIds }) {
-			id
-			mealName
-		}
-	}
+  query GetMealSuggestions($excludeIds: [ID!]!) {
+    meals(filter: { filterType: EXCLUDE, ids: $excludeIds }, limit: 9) {
+      id
+      mealName
+    }
+  }
 `
 
 const MealSuggestions = ({ keptMealIds, onKeepMeal }: Props) => {
-	const [excludedMealIds, setExcludedMealIds] = React.useState(keptMealIds)
-	function handleLoadSuggestions(previousMealIds: string[]) {
-		const ids = [...previousMealIds, ...keptMealIds]
-		setExcludedMealIds(ids)
-	}
+  const [excludedMealIds, setExcludedMealIds] = React.useState(keptMealIds)
+  function handleLoadSuggestions(previousMealIds: string[]) {
+    const ids = [...previousMealIds, ...keptMealIds]
+    setExcludedMealIds(ids)
+  }
 
-	return (
-		<div>
-			<Query<{ meals: MealType[] }, { excludeIds: string[] }>
-				query={query}
-				variables={{
-					excludeIds: excludedMealIds
-				}}
-			>
-				{({ loading, error, data }) => {
-					if (loading) {
-						return <div>Loading...</div>
-					}
+  return (
+    <div>
+      <Query<{ meals: MealType[] }, { excludeIds: string[] }>
+        query={query}
+        variables={{
+          excludeIds: excludedMealIds,
+        }}
+      >
+        {({ loading, error, data }) => {
+          if (loading) {
+            return <div>Loading...</div>
+          }
 
-					if (error) {
-						return <div>There was an error</div>
-					}
+          if (error) {
+            return <div>There was an error</div>
+          }
 
-					if (data == null) {
-						return <div>There are no meals to show</div>
-					}
+          if (data == null) {
+            return <div>There are no meals to show</div>
+          }
 
-					return (
-						<React.Fragment>
-							<div>
-								<Button
-									onClick={() =>
-										handleLoadSuggestions(data.meals.map(m => m.id))
-									}
-								>
-									Get More Suggestions
-								</Button>
-							</div>
-							<MealTileGrid>
-								{data.meals.map(meal => {
-									const isSelected = keptMealIds.includes(meal.id)
-									return (
-										<MealTile
-											style={{
-												backgroundColor: 'tomato',
+          return (
+            <React.Fragment>
+              <div>
+                <Button onClick={() => handleLoadSuggestions(data.meals.map(m => m.id))}>Get More Suggestions</Button>
+              </div>
+              <MealTileGrid>
+                {data.meals.map(meal => {
+                  const isSelected = keptMealIds.includes(meal.id)
+                  return (
+                    <MealTile
+                      style={{
+                        backgroundColor: 'tomato',
 
-												opacity: isSelected ? 0.4 : 1,
-												cursor: isSelected ? 'not-allowed' : 'pointer'
-											}}
-											key={meal.id}
-											onClick={() => {
-												if (isSelected) return
-												onKeepMeal(meal.id)
-											}}
-										>
-											<Meal meal={meal} />
-										</MealTile>
-									)
-								})}
-							</MealTileGrid>
-						</React.Fragment>
-					)
-				}}
-			</Query>
-		</div>
-	)
+                        opacity: isSelected ? 0.4 : 1,
+                        cursor: isSelected ? 'not-allowed' : 'pointer',
+                      }}
+                      key={meal.id}
+                      onClick={() => {
+                        if (isSelected) return
+                        onKeepMeal(meal.id)
+                      }}
+                    >
+                      <Meal meal={meal} />
+                    </MealTile>
+                  )
+                })}
+              </MealTileGrid>
+            </React.Fragment>
+          )
+        }}
+      </Query>
+    </div>
+  )
 }
 
 export default MealSuggestions
