e25e46bef58d3940d04995b86cdb38471de2c45d
Mike Joyce <mikejoyce19@gmail.com>
2019-06-02 09:20:30 -0500

    Add Auth to test if it works (#2)

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import 'typeface-roboto'
 import './global.css'
+import 'react-netlify-identity-widget/styles.css' // styling for login widget
+import { IdentityContextProvider } from 'react-netlify-identity-widget'
 import MealSelectPage from './pages/MealSelect/MealSelectPage'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import HomePage from './pages/Home/HomePage'
@@ -11,20 +13,28 @@ import MealPlansPage from './pages/MealPlans/MealPlansPage'
 import SharedMealPlan from './pages/MealPlan/SharedMealPlan'
 import Nav from './shared/Nav'
 
+const url = process.env.REACT_APP_NETLIFY_URL || 'FAKE_URL'
+
 const App = () => {
 	return (
-		<Router>
-			<Nav>
-				<Switch>
-					<Route path="/" exact component={HomePage} />
-					<Route path="/build" exact component={MealSelectPage} />
-					<Route path="/plan" exact component={MealPlansPage} />
-					<Route path="/plan/share" exact component={SharedMealPlan} />
-					<Route path="/plan/:planId" exact component={MealPlanPage} />
-					<Route path="/repositioning" exact component={MealPositioningPage} />
-				</Switch>
-			</Nav>
-		</Router>
+		<IdentityContextProvider url={url}>
+			<Router>
+				<Nav>
+					<Switch>
+						<Route path="/" exact component={HomePage} />
+						<Route path="/build" exact component={MealSelectPage} />
+						<Route path="/plan" exact component={MealPlansPage} />
+						<Route path="/plan/share" exact component={SharedMealPlan} />
+						<Route path="/plan/:planId" exact component={MealPlanPage} />
+						<Route
+							path="/repositioning"
+							exact
+							component={MealPositioningPage}
+						/>
+					</Switch>
+				</Nav>
+			</Router>
+		</IdentityContextProvider>
 	)
 }
 
